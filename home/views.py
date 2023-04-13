0e70eef7559704a7ec7bdc9ec93d064cca7e4e26
Pavel Savastseika <savaspavel@gmail.com>
2023-04-13 16:16:49 +0400

    add secret page, which requires logging

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from datetime import datetime
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
     return render(request=request, template_name='home/welcome.html', context={'today': datetime.today()})
+
+
+@login_required(login_url='/admin')
+def secret(request):
+    return render(request=request, template_name='home/secret.html')
