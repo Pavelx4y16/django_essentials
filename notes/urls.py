af22384d29e7451b63237c1d531005f92f7eeb6b
Pavel Savastseika <savaspavel@gmail.com>
2023-04-13 21:29:35 +0400

    add parameterized view

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from notes import views
 
 urlpatterns = [
-    path('list_notes', views.list_notes)
+    path('notes', views.list_notes),
+    path('notes/<int:pk>', views.note_details)
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,3 +6,8 @@ from notes.models import Notes
 def list_notes(request):
     all_notes = Notes.objects.all()
     return render(request, template_name='notes/list_notes.html', context={'notes': all_notes})
+
+
+def note_details(request, pk):
+    note = Notes.objects.get(pk=pk)
+    return render(request, template_name='notes/note_details.html', context={'note': note})
