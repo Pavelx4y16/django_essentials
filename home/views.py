cc5e1349b2f31994ff3ce30ed8f3a1b7f71328a2
Pavel Savastseika <savaspavel@gmail.com>
2023-04-17 13:20:12 +0400

    add Logout page

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 
 
-class LoginView(LoginView):
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
+
+class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
+
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
