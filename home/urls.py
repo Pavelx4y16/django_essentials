f089ab7e8332f2abfed64070ca8d60531a31dd34
Pavel Savastseika <savaspavel@gmail.com>
2023-04-17 14:42:12 +0400

    add SignUp page. Add Navigation bar

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
     path('secret', views.SecretView.as_view()),
-    path('login', views.LoginInterfaceView.as_view()),
-    path('logout', views.LogoutInterfaceView.as_view())
+    path('login', views.LoginInterfaceView.as_view(), name="login"),
+    path('logout', views.LogoutInterfaceView.as_view(), name="logout"),
+    path('signup', views.SignupView.as_view(), name="signup")
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from datetime import datetime
 
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, CreateView
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = "home/signup.html"
+    success_url = '/login'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 
 class LogoutInterfaceView(LogoutView):
